b258e1fc03dc0c16e67ce2475b310f0a3993ae4b
Anuj kumar sharma <141046905+anuj308@users.noreply.github.com>
2025-08-08 18:20:34 +0530

    vercel config files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),
-],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
+      }
+    }
+  },
+  base: './'
 })
